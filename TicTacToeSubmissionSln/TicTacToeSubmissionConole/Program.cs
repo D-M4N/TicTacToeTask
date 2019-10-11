7b300f97c0ed9e40ed1a7e65d3c11f1fb73b88c9
D-M4N <56065975+D-M4N@users.noreply.github.com>
2019-10-11 11:42:28 +0200

    UPLOAD

--- TicTacToeSubmissionSln/TicTacToeSubmissionConole/Program.cs
+++ TicTacToeSubmissionSln/TicTacToeSubmissionConole/Program.cs
@@ -10,18 +10,56 @@ namespace TicTacToeSubmissionConole
 
             Console.SetCursorPosition(10,2);
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine("Welcome to Tic Tac Toe");
+            Console.WriteLine("WELKOME TO MORTAL Tic Tac KOMBAT");
+
+                Console.SetCursorPosition(1, 4);
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("COLUMN: |  0  |  1  |  2  |");
+
+            Console.SetCursorPosition(1, 5);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("________|     |     |     |");
+
+                Console.SetCursorPosition(1, 7);
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("ROW: 0  |");
+
+            Console.SetCursorPosition(1, 8);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("_________");
+
+                Console.SetCursorPosition(1, 10);
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("ROW: 1  |");
+
+            Console.SetCursorPosition(1, 11);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("_________");
+
+                Console.SetCursorPosition(1, 13);
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write("ROW: 2  |");
+
+            Console.SetCursorPosition(1, 14);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("_________");
+
+
+
 
             var ticTacToe = new TicTacToe();
 
-            ticTacToe.Run();
+            ticTacToe.PlayerOne();
+
+            
+            ticTacToe.PlayerTwo();
 
 
             Console.ForegroundColor = oldColor ;
 
 
-            Console.SetCursorPosition(20, 25);
-            Console.WriteLine("Thank you for playing");
+            Console.SetCursorPosition(20, 31);
+            Console.WriteLine("MORTAL TIC TAC KOMBAT TOURNAMENT HAS ENDED");
             Console.ReadLine();
         }
     }
--- TicTacToeSubmissionSln/TicTacToeSubmissionConole/TicTacToe.cs
+++ TicTacToeSubmissionSln/TicTacToeSubmissionConole/TicTacToe.cs
@@ -12,36 +12,79 @@ namespace TicTacToeSubmissionConole
 
         public TicTacToe()
         {
-            _boardRenderer = new TicTacToeConsoleRenderer(10,6);
+            _boardRenderer = new TicTacToeConsoleRenderer(10, 6);
             _boardRenderer.Render();
+
+            
         }
 
 
-        public void Run()
+        public void PlayerOne()
         {
 
             // FOR ILLUSTRATION CHANGE TO YOUR OWN LOGIC TO DO TIC TAC TOE
+            Console.ForegroundColor = ConsoleColor.Yellow;
 
             Console.SetCursorPosition(2, 19);
 
-            Console.Write("Player X");
-
+            Console.Write("SCORPION IS \"X\"");
+                                                            
             Console.SetCursorPosition(2, 20);
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine();
 
             Console.Write("Please Enter Row: ");
             var row = Console.ReadLine();
 
             Console.SetCursorPosition(2, 22);
-
+            
+            Console.WriteLine();
 
             Console.Write("Please Enter Column: ");
             var column = Console.ReadLine();
 
 
             // THIS JUST DRAWS THE BOARD (NO TIC TAC TOE LOGIC)
-            _boardRenderer.AddMove(int.Parse(row), int.Parse(column), PlayerEnum.X, true);            
+            _boardRenderer.AddMove(int.Parse(row), int.Parse(column), PlayerEnum.X, true);
+
+            
 
         }
 
+        public void PlayerTwo()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            Console.SetCursorPosition(2, 25);
+
+            Console.Write("SUB-ZERO IS \"O\"");
+
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.SetCursorPosition(2, 26);
+
+            Console.WriteLine();
+
+            Console.Write("Please Enter Row: ");
+            var row = Console.ReadLine();
+
+            Console.SetCursorPosition(2, 28);
+
+            Console.WriteLine();
+
+            Console.Write("Please Enter Column: ");
+            var column = Console.ReadLine();
+
+
+            _boardRenderer.AddMove(int.Parse(row), int.Parse(column), PlayerEnum.O, true);
+
+            /*if (pos[1] == "O" && pos[2] == "O" && pos[3] == "O")
+            {
+                return true;
+            }*/
+
+        }
     }
 }
